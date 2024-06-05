78ce63dda2c157a9615fcf31e39e13a203799347
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-06-05 13:02:04 +0300

    importing and initialising the openai api

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
+import { OpenAI } from "openai";
 
 const app = express();
-
 const port = process.env.PORT || 3005;
+
+// assigning apikey
+const apikey = process.env.VITE_OPEN_AI_KEY;
+const openai = new OpenAI({ apiKey: apiKey });
 
 app.use(cors());
 app.use(express.json());
